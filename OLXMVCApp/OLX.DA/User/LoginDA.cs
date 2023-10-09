1a0592dab3a148564e574e5906a17b5ff6e54c43
rushi.rajwade <rajwderishi17@gmail.com>
2023-10-09 16:07:39 +0530

    added user data access

---
+++ OLXMVCApp/OLX.DA/User/LoginDA.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OLX.Models;
+
+namespace OLX.DA.User
+{
+    public class LoginDA
+    {
+
+        public bool authLogin(UsersModel model, out string validationmsg)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString());
+            string q = "select count(*) from Users where userEmail=@userEmail";
+            SqlCommand cmd = new SqlCommand(q, conn);
+            cmd.Parameters.AddWithValue("@userEmail", model.userEmail);
+
+            conn.Open();
+            int count = (int)cmd.ExecuteScalar();
+            conn.Close();
+            if (count == 0)
+            {
+                validationmsg = "UserEmail doesn't Exist";
+                return false;
+            }
+
+
+
+
+            string query = "select count(*) from Users where userEmail=@userEmail and Password=@userPassword";
+            cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@userEmail", model.userEmail);
+            cmd.Parameters.AddWithValue("@userPassword", model.userPassword);
+            conn.Open();
+            count = (int)cmd.ExecuteScalar();
+            conn.Close();
+            validationmsg = count == 0 ? "invalid credintials" : string.Empty;
+            if (count > 0)
+            {
+                bool isadmin = IsAdmin(model.userEmail);
+                if (isadmin)
+                {
+                    return true;
+                }
+            }
+            return count > 0;
+            //if (rows == 0)
+            //{  
+
+
+            //}
+            ////return false;
+            //validationmsg = "Invalid Credentials ";
+            //return rows>0 ;
+            //SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            //DataTable dataTable = new DataTable();
+
+        }
+        public bool IsAdmin(string userEmail)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString());
+            string query = "select role from users where userEmail=@userEmail and Role='Admin'";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@userEmail", userEmail);
+            conn.Open();
+            string role = (string)cmd.ExecuteScalar();
+            conn.Close();
+            return role == "Admin";
+        }
+
+    }
+}
