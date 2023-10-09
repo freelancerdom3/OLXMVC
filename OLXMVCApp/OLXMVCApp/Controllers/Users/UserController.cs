ae3af1318a1e81764ab82de60e603df4ae17f6c4
rushi.rajwade <rajwderishi17@gmail.com>
2023-10-09 18:47:16 +0530

    Modified user controller

--- OLXMVCApp/OLXMVCApp/Controllers/Users/UserController.cs
+++ OLXMVCApp/OLXMVCApp/Controllers/Users/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.SqlServer.Server;
@@ -13,6 +15,7 @@ namespace OLXMVCApp.Controllers.Users
 {
     public class UserController : Controller
     {
+        LoginDA access = new LoginDA();
         // GET: User
         public ActionResult Index()
         {
@@ -33,7 +36,7 @@ namespace OLXMVCApp.Controllers.Users
         [HttpPost]
         public ActionResult login(UsersModel loginModel)
         {
-            LoginDA access = new LoginDA();
+            
             bool result = access.authLogin(loginModel, out string msg);
             if (result)
             {
@@ -61,6 +64,90 @@ namespace OLXMVCApp.Controllers.Users
             }
 
 
+        }
+
+        public ActionResult sendotp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult sendotp(string Mobileno)
+        {
+           
+            bool ismobile = access.MobileNumberExists(Mobileno);
+            if (!ismobile)
+            {
+                return Json(new { success = false, message = "Invalid phone number format." });
+            }
+            Random rand = new Random();
+            int value = rand.Next(100001, 999999);
+            //string address = "+918237382320";
+            string otp = $"Your otp is:{value}";
+
+
+            int userid = access.GetUserIdByMobileNumber(Mobileno);
+            DateTime expiretime = DateTime.Now.AddMinutes(5);
+            access.InsertOtp(userid, value, expiretime);
+
+            using (var wb = new WebClient())
+            {
+                byte[] response = wb.UploadValues("https://api.textlocal.in/send/", new NameValueCollection()
+                {
+                    {"apikey","NTA2ODRhNGU0ZDUyNmY0MzQ2NmQ1YTczNjQ1YTM2N2E=" },
+                    {"numbers","91"+Mobileno },
+                    {"sender","TXTLCL" }
+                });
+                string result = System.Text.Encoding.UTF8.GetString(response);
+                Session["otp"] = value;
+
+            }
+            // return RedirectToAction("verify", "User");
+            // return Json(new { success = true, message = "OTP sent successfully." });
+            return Json(10);
+        }
+        public ActionResult MatchOtp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult MatchOtp(int LoginOtp)
+        {
+
+            //LoginModel loginModel = new LoginModel();
+
+           
+
+            int userid = access.getidfromOtp(LoginOtp, out string msg);
+
+            if (userid > 0)
+            {   //string stored = user.GetOtp(userid);
+
+                int stored = access.GetOtp(userid, out string message);
+                //bool otp = user.verifyOTP(userotp,out stored);
+                if (LoginOtp == stored)
+                {
+                    return Json(1);
+                    //return Json(new { message = "login" });
+                }
+                else
+                {
+                    //ViewBag.userid = message;
+                    return Json(0);
+                }
+                //return Json(1);}
+
+
+
+
+            }
+            else
+            {
+               // ViewBag.msg = msg;
+                return Json(2);
+               // return View("verify");
+            }
         }
 
         public ActionResult Logout()
