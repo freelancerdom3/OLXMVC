d8101a4bf9273183973e2214fc9ad66b9e53979a
dhriti.patel <dhriti.patel@DT239>
2023-10-16 13:29:08 +0530

    added

--- OLXMVCApp/OLXMVCApp/Controllers/HomeController.cs
+++ OLXMVCApp/OLXMVCApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OLX.DA.User;
+using OLX.Models.User;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,7 @@ namespace OLXMVCApp.Controllers
 {
     public class HomeController : Controller
     {
+        UserBuyDA dataAccess = new UserBuyDA();
         public ActionResult Index()
         {
             return View();
@@ -27,6 +30,37 @@ namespace OLXMVCApp.Controllers
             return View();
         }
 
-        
+
+        public ActionResult newfilter(int? categoryid, int? subcategoryid, int? stateid, int? cityid, int? areaid, decimal? minprice, decimal? maxprice, int? advertiseId)
+        {
+            List<UserBuyModel> list = dataAccess.newFilter(categoryid, subcategoryid, stateid, cityid, areaid, minprice, maxprice, advertiseId);
+
+            return View(list);
+        }
+
+
+        public ActionResult showcatsub()
+        {
+          
+            List<UserBuyModel> categorywithsub = dataAccess.GetCategoryWithSubcategories();
+
+            return View(categorywithsub);
+        }
+
+        public ActionResult showAdvertiseDetails(int advertiseId)
+        {
+      
+            IEnumerable<UserBuyModel> advertise = dataAccess.GetAdvertiseById(advertiseId);
+
+            return View(advertise);
+        }
+
+        public ActionResult showLocation()
+        {
+
+            List<UserBuyModel> location = dataAccess.GetLocation();
+
+            return View(location);
+        }
     }
 }
